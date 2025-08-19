4a6ed6cd1189e458574cbb6c5e6907ba1ed95b4a
mfyuu <83203852+ve1997@users.noreply.github.com>
2025-08-19 22:33:35 +0900

    feat: add ts file

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+export const add = (a: number, b: number) => a + b;
+export const subtract = (a: number, b: number) => a - b;
+export const multiply = (a: number, b: number) => a * b;
+export const divide = (a: number, b: number) => a / b;
+export const power = (a: number, b: number) => a ** b;
+export const sqrt = (a: number) => Math.sqrt(a);
+export const cbrt = (a: number) => Math.cbrt(a);
+export const abs = (a: number) => Math.abs(a);
+export const sin = (a: number) => Math.sin(a);
+export const cos = (a: number) => Math.cos(a);
